5afe1b3cf694ee0648a54ea6f83bd45486647c4d
JhonnWdt <joniwendt46@gmail.com>
2025-03-05 19:08:12 +0000

    novato

--- xadrez.c
+++ xadrez.c
@@ -7,9 +7,56 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int bispo = 0, torre = 0, rainha, escolha;
+
+    printf("Escolha qual peça deseja mover:\n");
+    printf("\n1. Bispo\n");
+    printf("2. Torre\n");
+    printf("3. Rainha\n");
+    printf("Escolha o número da peça:\n");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+        printf("\nMovimento do Bispo:\n");
+
+        while (bispo < 5)
+        {
+            printf("Frente, Direita\n");
+            bispo++;
+        }
+        
+        break;
+
+    case 2:
+        printf("\nMovimento da Torre:\n");
+
+        do
+        {
+            printf("Direita\n");
+            torre++;
+        } while (torre < 5);
+
+        break;
+
+    case 3:
+        printf("\nMovimento da Rainha:\n");
+
+        for (rainha = 0; rainha < 8; rainha++)
+        {
+            printf("Direita\n");
+        }
+
+        break;
+        
+    default:    printf("\nOpção inválida, por favor insira um número de 1 a 3\n");
+        break;
+    }
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
