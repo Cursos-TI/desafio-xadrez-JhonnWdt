bd1b0002ef4b629fcfb6c5aeeae47dc0c10dfca3
JhonnWdt <joniwendt46@gmail.com>
2025-03-08 22:17:26 +0000

    mestre incompleto

--- xadrez.c
+++ xadrez.c
@@ -1,75 +1,74 @@
 #include <stdio.h>
 
-int main() {
-    int bispo = 0, torre = 0, rainha, cavalo = 0, escolha;
-
-    printf("♝♜♛♞ Movimento das peças de Xadrez ♞♛♜♝\n");
-    printf("\nEscolha qual peça deseja mover:\n");
-    printf("\n1. Bispo ♝\n");
-    printf("2. Torre ♜\n");
-    printf("3. Rainha ♛\n");
-    printf("4. Cavalo ♞\n");
-    printf("Escolha o número da peça:\n");
-    scanf("%d", &escolha);
-
-    switch (escolha)
-    {
-    case 1:
-     // Implementação de Movimentação do Bispo
-        printf("\nMovimento do Bispo ♝ :\n");
-
-        while (bispo < 5)
-        {
-            printf("Cima\n");
-            printf("Direita\n");
-            bispo++;
-        }
-        
-        break;
-
-    case 2:
-        // Implementação de Movimentação da Torre
-        printf("\nMovimento da Torre ♜ :\n");
-
-        do
-        {
-            printf("Direita\n");
-            torre++;
-        } while (torre < 5);
-
-        break;
-
-    case 3:
-    // Implementação de Movimentação da Rainha
-        printf("\nMovimento da Rainha ♛ :\n");
-
-        for (rainha = 0; rainha < 8; rainha++)
-        {
-            printf("Direita\n");
-        }
-
-        break;
-
-    case 4:
-    // Implementação de Movimento do Cavalo
-        printf("\nMovimento do Cavalo ♞\n");
-
-        for (cavalo; cavalo <= 1; cavalo++)
-        {
-            while (cavalo < 2)
-            {
-                printf("Baixo\n");
-                cavalo++;
-            }
-            printf("Esquerda\n");
-        }
-
-        break;
-    
-    default:    printf("\nOpção inválida, por favor insira um número de 1 a 4\n");
-        break;
+void movBispo(int passos){
+    if (passos > 0) {
+        printf("Cima\n", passos);
+        printf("Direita\n", passos);
+        movBispo(passos - 1);
     }
-    
+}
+
+void movTorre(int passos){
+    if (passos > 0) {
+        printf("Direita\n", passos);
+        movTorre(passos - 1);
+    }
+}
+
+void movRainha(int passos){
+    if (passos > 0) {
+        printf("Esquerda\n", passos);
+        movRainha(passos - 1);
+    }
+}
+
+int main() {
+    int bispo = 5, torre = 5, rainha = 8, cavalo = 0, escolha;
+
+    do {
+        printf("\n♜ Movimento das peças de Xadrez ♜\n");
+        printf("\nEscolha qual peça deseja mover:\n");
+        printf("\n1. Bispo ♝\n");
+        printf("2. Torre ♜\n");
+        printf("3. Rainha ♛\n");
+        printf("4. Cavalo ♞\n");
+        printf("5. Sair do Programa\n");
+        printf("Escolha o número da peça:\n");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+            case 1:
+                printf("\nMovimento do Bispo ♝ :\n");
+                movBispo(bispo);
+                break;
+
+            case 2:
+                printf("\nMovimento da Torre ♜ :\n");
+                movTorre(torre);
+                break;  // Corrigido
+
+            case 3:
+                printf("\nMovimento da Rainha ♛ :\n");
+                movRainha(rainha);
+                break;
+
+            case 4:
+                printf("\nMovimento do Cavalo ♞\n");
+                for (int i = 0; i < 2; i++) {
+                    printf("Baixo\n");
+                }
+                printf("Esquerda\n");
+                break;
+
+            case 5: 
+                printf("Saindo do Programa...\n");
+                break;
+
+            default:
+                printf("\nOpção inválida, por favor insira um número de 1 a 5\n");
+                break;
+        }
+    } while (escolha != 5);
 
     return 0;
 }
